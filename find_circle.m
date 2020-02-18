a78017f480d0d7f96e9974c4d6a2a27653423002
dohyuck <ehgur1652@naver.com>
2020-02-18 17:20:51 +0900

    github tutorial

---
+++ find_circle.m
@@ -0,0 +1,23 @@
+rgb = imread('C:\±³ÁÂº¯À§\014\Img_014_20191111_140102.jpg');
+imshow(rgb)
+
+% d = imdistline;
+% delete(d)
+
+% gray_image = rgb2gray(rgb);
+% imshow(gray_image)
+
+
+% [centers,radii] = imfindcircles(rgb,[15 30],'ObjectPolarity','dark', ...
+%     'Sensitivity',0.9);
+% 
+% length(centers)
+
+% delete(h)  % Delete previously drawn circles
+% h = viscircles(centers,radii);
+
+[centers,radii] = imfindellipse(rgb,[20 40],'ObjectPolarity','bright', ...
+          'Sensitivity',0.9,'Method','twostage');
+
+delete(h)
+h = viscircles(centers,radii);
